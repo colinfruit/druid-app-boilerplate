724bc802973040bc1f3f8834b43b3cbde40f84ae
colinfruit <17092461+colinfruit@users.noreply.github.com>
2021-03-05 23:10:42 -0500

    add basic template

---
+++ README.md
@@ -0,0 +1,1 @@
+# druid-app-boilerplate
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use druid::widget::prelude::*;
+use druid::widget::{Flex, Label, CrossAxisAlignment, MainAxisAlignment};
+use druid::{AppLauncher, WindowDesc, Size};
+
+pub fn main() {
+    // describe the main window
+    let main_window = WindowDesc::new(build_root_widget)
+        .show_titlebar(false)
+        .resizable(false)
+        .with_min_size(Size::ZERO)
+        // TODO: there's definitely a better way of doing this.
+        .window_size((360., 648.))
+        .title("Druid app boilerplate");
+
+    // start the application. Here we pass in the application state.
+    AppLauncher::with_window(main_window)
+        .launch(())
+        .expect("Failed to launch application");
+}
+
+fn build_root_widget() -> impl Widget<()> {
+    // arrange the two widgets vertically, with some padding
+    Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Center)
+        .main_axis_alignment(MainAxisAlignment::Center)
+        .must_fill_main_axis(true)
+        .with_child(Label::new("Hello PinePhone!"))
+}
